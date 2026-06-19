35c23eed9bf8fac4cd5f78d93ce0f378ec71af7c
Stephano Suarez <suarezstephano07@gmail.com>
2026-06-18 19:54:25 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,1 +1,5 @@
+const SUPABASE_URL = "https://cibduyalitpcbtocoerq.supabase.co";
 
+const SUPABASE_KEY = "sb_publishable_i1XkQzWyprVm9YdV0TPejw_qRKaW8lp";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
